de462129ba59f032609f077033231f980cb634fe
ourflashservice <ourflashservice@gmail.com>
2023-08-20 17:04:41 +0100

    jkbdhgrie

--- react-simple-captcha.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'react-simple-captcha' {
-    export function loadCaptchaEnginge(num: number): void;
-    export function LoadCanvasTemplate(): any;
-    export function LoadCanvasTemplateNoReload(): void;
-    export function validateCaptcha(): boolean;
-  }
-  
