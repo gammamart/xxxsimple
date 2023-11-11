63738603ed2618511fcfb6d121de0a4072e18d79
ourflashservice <ourflashservice@gmail.com>
2023-11-11 02:30:43 +0100

    added analytics

--- pages/dashboard/fund-wallet.tsx
+++ pages/dashboard/fund-wallet.tsx
@@ -247,7 +247,7 @@ const ContinueButton = styled.button<FundButtonProps>`
 const USDTWalletAddressBox = styled.button`
   height: 50px;
   width: 400px;
-  background: green;
+  background: #ffffff10;
   border: 1px solid #ffffff50;
   color: #ffffffd2;
   cursor: pointer;
