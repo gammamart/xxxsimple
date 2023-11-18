7064ae6aa5b7e61211371c4bc09d0d44ea043da9
ourflashservice <ourflashservice@gmail.com>
2023-11-18 16:17:23 -0500

    maintaince

--- middleware.ts
+++ middleware.ts
@@ -11,7 +11,7 @@ export default function middleware(request) {
 
   console.log(url)
 
-  const maintaining = true;
+  const maintaining = false;
 
   // privatizating AUTHENTICATED ROUTES
   if (maintaining) {
