325d4d8ce853890f26697799e800441bc06682b0
ourflashservice <ourflashservice@gmail.com>
2023-12-09 16:41:54 +0100

    toaster position

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -444,6 +444,20 @@ const Body = styled.div`
     &:focus {
       outline: 2px solid #41465178;
     }
+
+    ::-webkit-scrollbar {
+      width: 12px; /* Set the width of the scrollbar track */
+    }
+
+    ::-webkit-scrollbar-thumb {
+      background-color: #888; /* Set the color of the scroll knob */
+      border-radius: 6px; /* Set the border radius of the knob */
+    }
+
+    ::-webkit-scrollbar-track {
+      background-color: #f1f1f1; /* Set the color of the scrollbar track */
+      border-radius: 6px; /* Set the border radius of the track */
+    }
   }
 
   & textarea:first-child {
