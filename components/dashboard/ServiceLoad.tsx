e01986faaa0b9e3931d7e4ccf6f8f599065771cd
ourflashservice <ourflashservice@gmail.com>
2023-12-13 00:45:39 +0100

    adding more information to the load status

--- components/dashboard/ServiceLoad.tsx
+++ components/dashboard/ServiceLoad.tsx
@@ -45,9 +45,9 @@ const ServiceLoad: React.FC<ServiceLoadProps> = ({ status }) => {
   };
 
   const status_text: any = {
-    LL: "Low Load",
-    ML: "Moderate Load",
-    OL: "High Load",
+    LL: "Low Load: Smooth sailing",
+    ML: "Moderate Load: Steady traffic",
+    OL: "High Load: Expect delays",
   };
 
   const statusAnimation: any = {
