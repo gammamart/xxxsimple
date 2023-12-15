fb9b0fd9357c6acecf3a4205ee0b5389ad94d640
ourflashservice <ourflashservice@gmail.com>
2023-12-15 02:56:27 +0100

    changing the membership icon

--- components/global/Navbar.tsx
+++ components/global/Navbar.tsx
@@ -120,7 +120,7 @@ const Navbar = () => {
         </NavButton>
         <NavButton href="../dashboard/upgrade" ref={contactUsButton}>
           <span>
-            <PiShieldCheck size={18} />
+            <PiShieldCheck size={20} />
           </span>{" "}
           <p>Membership</p>
         </NavButton>
