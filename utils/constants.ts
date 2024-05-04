07702299de8e35adc3455f1a814df63c4a56a767
gammamart <130505967+gammamart@users.noreply.github.com>
2024-05-04 07:39:07 +0100

    wallet

--- utils/constants.ts
+++ utils/constants.ts
@@ -2,26 +2,26 @@ export const staticWallet: any = [
     {
       name: "BTC",
       type: "BTC",
-      address: "BtcBksSTjNM7EEab2XgNie7jYKriXYKWmK",
+      address: "bc1qvznay8prurkv8t5wx07ja5j8lwalptt8s3yzw7",
     },
     {
       name: "ETH",
       type: "ETH",
-      address: "EthBksSTjNM7EEab2XgNie7jYKriXYKWmK",
+      address: "0x73eA489cb0cc4fbB8DA6Cbd5d2ff44a378e43Dc9",
     },
     {
       name: "USDT(TRC20)",
       type: "trc20",
-      address: "TRCBksSTjNM7EEab2XgNie7jYKriXYKWmK",
+      address: "TWyBksSTjNM7EEab2XgNie7jYKriXYKWmK",
     },
     {
       name: "USDT(ERC20)",
       type: "USDT(ERC20)",
-      address: "ERCBksSTjNM7EEab2XgNie7jYKriXYKWmK",
+      address: "0x73eA489cb0cc4fbB8DA6Cbd5d2ff44a378e43Dc9",
     },
     {
       name: "LTC",
       type: "LTC",
-      address: "LTCBksSTjNM7EEab2XgNie7jYKriXYKWmK",
+      address: "ltc1qpe45jdfw9leh2qadaraun0mhep4s6m4zs2sy22",
     },
   ];
