5adbc32a275333dab955163a15abfc714a73f4bb
ourflashservice <ourflashservice@gmail.com>
2024-10-26 19:03:20 +0100

    c ch

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,28 +1,27 @@
 export const staticWallet: any = [
-    {
-      name: "BTC",
-      type: "BTC",
-      address: "bc1qvznay8prurkv8t5wx07ja5j8lwalptt8s3yzw7",
-    },
-    {
-      name: "ETH",
-      type: "ETH",
-      address: "0x73eA489cb0cc4fbB8DA6Cbd5d2ff44a378e43Dc9",
-    },
-    {
-        name: "LTC",
-        type: "LTC",
-        address: "ltc1qpe45jdfw9leh2qadaraun0mhep4s6m4zs2sy22",
-      },
-    {
-      name: "USDT(TRC20)",
-      type: "trc20",
-      address: "TWyBksSTjNM7EEab2XgNie7jYKriXYKWmK",
-    },
-    {
-      name: "USDT(ERC20)",
-      type: "USDT(ERC20)",
-      address: "0x73eA489cb0cc4fbB8DA6Cbd5d2ff44a378e43Dc9",
-    },
-    
-  ];
+  {
+    name: "BTC",
+    type: "BTC",
+    address: "bc1q6lkgksre2zlz2aww66zpdyqg0az934razp0vp2",
+  },
+  {
+    name: "ETH",
+    type: "ETH",
+    address: "0x57aaE6eD965D75538Dc36A6D25876544167de85D",
+  },
+  {
+    name: "LTC",
+    type: "LTC",
+    address: "ltc1q8j8le358vx8eunxhy3gvdnc7mxyxlrw067kzwc",
+  },
+  {
+    name: "USDT(TRC20)",
+    type: "trc20",
+    address: "TVNCgSGjh93ekd5wMnQX7dxcsXrLRXuVM6",
+  },
+  {
+    name: "USDT(ERC20)",
+    type: "USDT(ERC20)",
+    address: "0x57aaE6eD965D75538Dc36A6D25876544167de85D",
+  },
+];
