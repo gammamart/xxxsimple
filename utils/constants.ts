a7bd111fae1dca09ac561806699fe6fdce4af9c6
gammamart <gamma_mart1@proton.me>
2025-09-02 13:03:55 +0100

    wc

--- utils/constants.ts
+++ utils/constants.ts
@@ -2,26 +2,26 @@ export const staticWallet: any = [
   {
     name: "BTC",
     type: "BTC",
-    address: "bc1qsy6zdk2tsd5kuj6scn6ua3a5vawqmltm5km6gl",
+    address: "bc1qhl6qw00eex252fer4ww23u9vhn39y2s4vhkryc",
   },
   {
     name: "ETH",
     type: "ETH",
-    address: "0x1CB066D5aA56892806D95a1433e3Ebe5468fF438",
+    address: "0x011963243871e27EF2861604166a5A576916BB46",
   },
   {
     name: "LTC",
     type: "LTC",
-    address: "ltc1q80vtrjftkwlt0xxumatauqqg5dq6wl4rjrveux",
+    address: "ltc1qnskqkv00kv20mcvw9u9zu33judt6lrvt5rcgaq",
   },
   {
     name: "USDT(TRC20)",
     type: "trc20",
-    address: "TYJXQx6U6z7VQ2aBugdk3mjbJSwHErSbuW",
+    address: "TL8xnNAGk5agAFr7SdxGToe125HuQV5tuU",
   },
   {
     name: "USDT(ERC20)",
     type: "USDT(ERC20)",
-    address: "0x1CB066D5aA56892806D95a1433e3Ebe5468fF438",
+    address: "0x011963243871e27EF2861604166a5A576916BB46",
   },
 ];
