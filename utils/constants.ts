652bfbcaa291ad00ffc039985a3c63f903bb6ebc
gammamart <gamma_mart1@proton.me>
2025-09-12 12:37:39 +0100

    adj

--- utils/constants.ts
+++ utils/constants.ts
@@ -5,6 +5,11 @@ export const staticWallet: any = [
     address: "bc1qhl6qw00eex252fer4ww23u9vhn39y2s4vhkryc",
   },
   {
+    name: "XMR",
+    type: "XMR",
+    address: "43cq6w1oBiWRooSGQF4eB8RRRaixpTGbRSwwC9b9FsUmQZnn4C7nuVPFWmseELRTAWDJB9zYraNWqBuZCxsjyQDqEsQBwYY",
+  },
+  {
     name: "ETH",
     type: "ETH",
     address: "0x011963243871e27EF2861604166a5A576916BB46",
